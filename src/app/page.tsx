de4bdb35d808735ca5ac62784e605cd0d62b1b7f
José Viejo HUerta <Jviejo@users.noreply.github.com>
2024-10-09 08:12:05 +0200

    page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
 "use client"
 import Login from "@/components/login/Login";
 import SignUp from "@/components/login/Signup";
+import Image from "next/image";
 import { useState } from "react";
 
 export default function Home() {
   const [activeTab, setActiveTab] = useState('login');
   return (
-      <div className="flex justify-center  p-5">
-
-
+      <div className="flex justify-around  p-5">
+        <div className="w-1/4 flex flex-col items-center">
+          <Image src="/logo.png" alt="logo" width={100} height={100} />
+          <h1 className="text-2xl font-bold">Welcome to our website</h1>
+        </div>
          <div className="w-full max-w-md bg-white rounded-lg shadow-md overflow-hidden">
-           <div className="flex">
+           <div className="flex gap-2">
              <button
                className="flex-1 py-2 px-4 text-center bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
                onClick={() => setActiveTab('login')}
